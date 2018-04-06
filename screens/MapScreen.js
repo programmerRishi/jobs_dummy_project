3a7a88b702df7e55f523099f5ab66a3a27939c00
Rishi Sharma <rshsharma90@gmail.com>
2018-04-07 00:23:02 +0530

    Map screen edited

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  }
+  // arrow function as a callback donot need a '.bind(this)'
+  onRegionChangeComplete = (region) => {
+    console.log(region);
+    this.setState({ region });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          I am MapScreen
-        </Text>
+      <View style={{ flex: 1, marginTop: 15 }}>
+        <MapView
+         style={{ flex: 1 }}
+         region={this.state.region}
+         onRegionChangeComplete={this.onRegionChangeComplete}
+        />
       </View>
           );
         }
       }
-const styles = {
-        container: {
-          flex: 1,
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center',
-        },
-      };
+
 export default MapScreen;
